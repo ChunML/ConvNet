b95d7322d373182bc155a9a06b8a8ded00374a4e
ChunML <ahzhongmusic@gmail.com>
2016-09-19 00:51:01 +0900

    Fixing CIFAR-10 source for display test images in 3 channel format

--- CNN/lenet_cifar_10.py
+++ CNN/lenet_cifar_10.py
@@ -54,9 +54,14 @@ for i in np.random.choice(np.arange(0, len(test_label)), size=(30,)):
         pred = probs.argmax(axis=1)
 
         # Enlarge the image
-        image = cv2.resize(test_data[i][0], (96, 96), interpolation=cv2.INTER_LINEAR)
+        # image = cv2.resize(test_data[i][0], (96, 96), interpolation=cv2.INTER_LINEAR)
+        b = test_data[i, 0]
+        g = test_data[i, 1]
+        r = test_data[i, 2]
+        image = cv2.merge((b, g, r))
         cv2.putText(image, str(pred[0]), (5, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
 
         print("[INFO] Predicted: {}, Actual: {}".format(pred[0], np.argmax(test_label[i])))
+        cv2.namedWindow("Digit", cv2.WINDOW_NORMAL)
         cv2.imshow("Digit", image)
         cv2.waitKey(0)
